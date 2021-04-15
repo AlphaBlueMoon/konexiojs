6939ae9e6990f04fd129abe7a18ce50769ba7d29
Anna Frumy <81561135+AlphaBlueMoon@users.noreply.github.com>
2021-04-15 08:51:58 +0200

    Add files via upload

---
+++ JavaScript/Jour 2/jour02.js
@@ -0,0 +1,54 @@
+//01-Number//
+var integer = 102;
+var float = 13.9;
+console.log(integer);
+console.log(float);
+
+//02- Convert//
+var basic = 34;
+var springfield;
+var stringfied = basic.toString();
+
+console.log(stringfied);
+// 03 - Round
+var num = 1.5;
+
+var rounded = Math.round(num);
+
+console.log(rounded);
+
+// 04 - Arithmétique
+var test = 12;
+var bis = 5;
+
+console.log('Addition:', test + bis);
+
+console.log('Soustraction:', test - bis);
+
+console.log('Multiplication:', test * bis);
+
+console.log('Division:', test / bis);
+
+console.log('Puissance:', test ** bis);
+
+console.log('Reste:', test % bis);
+
+// 05 - Comparaison
+var test = 143;
+var bis = 219;
+
+console.log('> :', test > bis);
+
+console.log('< :', test < bis);
+
+console.log('>= :', test >= bis);
+
+console.log('<= :', test <= bis);
+
+console.log('== :', test == bis);
+
+console.log('=== :', test === bis);
+
+console.log('!= :', test != bis);
+
+console.log('!== :', test !== bis);
